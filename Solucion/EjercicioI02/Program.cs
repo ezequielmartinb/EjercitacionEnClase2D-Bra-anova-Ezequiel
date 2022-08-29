ba8e1a7572ff2abd74035bdce101ff55f9a57cff
ezequielmartinb <ezequielmartinb10@gmail.com>
2022-08-28 21:50:38 -0300

    Ejercitacion Primera Semana Incompleta (Me falta el ejercicio Ejercicio I03)

---
+++ Solucion/EjercicioI02/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace EjercicioI02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Realizar un programa que sume números enteros hasta que el usuario lo determine por medio de un mensaje "¿Desea continuar? (S/N)".
+            Crear la clase Validador y el método estático ValidarRespuesta, que validará el ingreso de respuestas.
+            El método devolverá un valor de tipo booleano, TRUE si se ingresó una 'S' y FALSE si se ingresó cualquier otro valor.
+            */
+
+            int numeroIngresado;
+            int acumuladorNumerosIngresados = 0;
+            bool resultadoParseNumeroIngresado;
+            bool deseaContinuar = true;
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine($"Ingrese un numero entero #{i + 1}");
+                resultadoParseNumeroIngresado = int.TryParse(Console.ReadLine(), out numeroIngresado);
+
+                if (resultadoParseNumeroIngresado)
+                {
+                    acumuladorNumerosIngresados += numeroIngresado;
+                    deseaContinuar = Validador.ValidarRespuesta();
+                }
+                else
+                {
+                    break;
+                }
+                i++;
+
+            } while (deseaContinuar == true);
+
+            if(resultadoParseNumeroIngresado)
+            {
+                Console.WriteLine($"La suma de los numeros ingresados es: {acumuladorNumerosIngresados}");
+            }
+            else
+            {
+                Console.WriteLine("NO SE INGRESO UN NUMERO ENTERO VALIDO");
+            }
+            
+
+        }
+    }
+}
