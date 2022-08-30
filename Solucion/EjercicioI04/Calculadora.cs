9e6428009e470fa1af093bbb8c4530980d1f5a08
ezequielmartinb <ezequielmartinb10@gmail.com>
2022-08-29 23:25:17 -0300

    Ejercicios en clase 29-08-2022

---
+++ Solucion/EjercicioI04/Calculadora.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioI04
+{
+    public class Calculadora
+    {
+        /*
+        Calcular(público) : Recibirá tres parámetros: 
+
+        el primer operando, 
+        el segundo operando y 
+        la operación matemática.
+        
+        El método devolverá el resultado de la operación.
+
+        Validar(privado): 
+        
+        Recibirá como parámetro el segundo operando.        
+        Este método se debe utilizar sólo cuando la operación elegida sea la DIVISIÓN.         
+        Este método devolverá true si el operando es distinto de cero.
+        */
+
+        public static double Calcular(double primerOperador, double segundoOperador, char operacionMatematica)
+        {
+            double resultado = 0;
+
+            switch (operacionMatematica)
+            {
+                case '+':
+                    resultado = primerOperador + segundoOperador;
+                    break;
+                case '-':
+                    resultado = primerOperador - segundoOperador;
+                    break;
+                case '*':
+                    resultado = primerOperador * segundoOperador;
+                    break;
+                case '/':
+                    if (Validar(segundoOperador))
+                    {
+                        resultado = primerOperador / segundoOperador;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No se puede dividir por 0");
+                    }
+                    break;
+            }
+
+            return resultado;
+        }
+
+        private static bool Validar(double segundoOperando)
+        {
+            bool resultado = false;
+
+            if (segundoOperando != 0)
+            {
+                resultado = true;
+            }
+
+            return resultado;
+        }
+        public static bool Validar(char operacionMatematica)
+        {
+            bool retorno = true;
+
+            if (operacionMatematica != '+' && operacionMatematica != '-' && operacionMatematica != '*' && operacionMatematica != '/')
+            {
+                retorno = false;
+            }
+
+            return retorno;
+        }
+        public static bool Validar()
+        {
+            bool validacion;
+            char respuestaIngresada;
+
+            Console.WriteLine("Desea continuar? S/N");
+            validacion = char.TryParse(Console.ReadLine(), out respuestaIngresada);
+
+            if (respuestaIngresada == 'S')
+            {
+                validacion = true;
+            }
+            else
+            {
+                validacion = false;
+            }
+
+            return validacion;
+
+        }
+    }
+
+}
